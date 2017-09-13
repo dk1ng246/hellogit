6e1787400a0bcd465e6b7932783e65646219937a
dk1ng246 <dk1ng246@users.noreply.github.com>
2017-09-13 16:02:14 -0400

    added files

--- main.cpp
+++ main.cpp
@@ -6,23 +6,25 @@ using namespace std;
 
 int main()
 {
-		
+	string employee;
+	double employeew;
+	
 	vector<Payroll>v1;
 	for(int i=0; i<4; i++)
 	{
 		cout << "Enter employee's name";
 		cin >> employee;
 		cout << "Enter employee's wage";
-		cin >> wage;
+		cin >> employeew;
 
-		Payroll newEmployee(employee, wage);
+		Payroll newEmployee(employee, employeew);
 		v1.push_back(newEmployee);
 	}
 	cout << endl;
 
 	for(auto v:v1)
 	{
-		cout << v.getemployee()<<" "<<v.getwage()<<endl;
+		cout << v.getemployee()<<" "<<v.getemployeew()<<endl;
 	}
 	cout << endl;
 
